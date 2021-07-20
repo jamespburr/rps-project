aa39828445873e744e0b93763f5102acb110203b
James Burrows <jburrows@milestechnologies.com>
2021-07-19 21:26:29 -0400

    Initial Attempt
    First attempt at completed project

--- rps/controller.js
+++ rps/controller.js
@@ -5,21 +5,39 @@ var game = new Game();
 function playGame() {
     //*********************************ADD CODE HERE *************************************/
     // Call the function game.newGame() to reset the counts.
+    game.newGame()
 
     //*********************************ADD CODE HERE *************************************/
     // Get the number of rounds from the user, and store it in a variable called numberOfRounds.
     // Use game.IsNumerOfRoundsValid() to validate the user input.
     // If the input is valid, use game.setNumberOfRounds(numberOfRounds) to set the number of rounds for the game 
     //  >> Else, try to get the number of rounds for the suer again
+   
+    let numberOfRounds = 0;
+    let isRoundsValid = false;
 
+    do {
+    numberOfRounds = parseInt(window.prompt('Enter number of rounds (1-5)'));
+    isRoundsValid = game.isNumberOfRoundsValid(numberOfRounds);
+    } while (!isRoundsValid);
+
+    game.setNumberOfRounds(numberOfRounds);
 
     //*********************************ADD CODE HERE *************************************/
-    // Call playRound() with a parameter of the round count (1 thru numberOfRounds) for each round 
+    // Call playRound() with a parameter of the round count (1 thru numberOfRounds) for each round
+    
+    for (let i = 0; i < numberOfRounds; i++){
+        
+        playRound(i)
+    }
 
+    
 
     //*********************************ADD CODE HERE *************************************/
     // Display the Final Score from the properties stored in the Game object
-
+    console.log('Player Score: ' + game.countOfPlayerWins)
+    console.log('Computer Score: ' + game.countOfComputerWins)
+    console.log('Ties: ' + game.countOfTies)
 }
 
 /**
@@ -29,17 +47,20 @@ function playGame() {
 function playRound(roundNumber) {
     //*********************************ADD CODE HERE *************************************/
     // Display the Current Score to the User
+    console.log('Player Score: ' + game.countOfPlayerWins)
+    console.log('Computer Score: ' + game.countOfComputerWins)
+    console.log('Ties: ' + game.countOfTies)
     
     //Prompt for Player Selection
-    var playerSelection = getPlayerSelection();
-    var round = new Round(playerSelection);
+    let playerSelection = getPlayerSelection();
+    let round = new Round(playerSelection);
 
     //Play the Round
-    var outcome = round.determineWinner();
+    let outcome = round.determineWinner();
 
     //*********************************ADD CODE HERE *************************************/
     // Display the Round Results from the properties stored in the Round object
-
+    console.log(outcome + ' wins')
 }
 
 
@@ -52,5 +73,18 @@ function getPlayerSelection() {
     
     //*********************************ADD CODE HERE *************************************/
     // Prompt, Validate and Return the player selection. This must be in the form of "rock", "paper", or "scissors" exactly
+
+    let playerSelection = ""
+    let selectionArray = Object.values(Selections)
+
+    do {
+        playerSelection = window.prompt('Enter rock, paper or scissors')
+        playerSelection = playerSelection.toLowerCase()
+    } while (!selectionArray.includes(playerSelection))
+
+    return playerSelection
+    
+
+    
 }
 
--- rps/round.js
+++ rps/round.js
@@ -29,6 +29,11 @@ class Round{
     getComputerSelection() {
         //*********************************ADD CODE HERE *************************************/
         // Use Math.floor and select a random Selections enumerator from above 
+        let selectionArray = Object.values(Selections)
+        let selection = Math.floor(Math.random()*3);
+        return selectionArray[selection]
+        
+        
     }
 
     /**
@@ -40,6 +45,37 @@ class Round{
     determineWinner() {
         //*********************************ADD CODE HERE *************************************/
         // Use this.playerSelection and this.computerSelection to return the appropriate outcome from the Outcomes enumerator above.
+        let outcome = "";
+        if (this.playerSelection == Selections.ROCK && this.computerSelection == Selections.SCISSORS) {
+            outcome = Outcomes.PLAYER_WINS
+            game.incrementPlayerWins()
+        } 
+        else if (this.playerSelection == Selections.ROCK && this.computerSelection == Selections.PAPER) {
+            outcome = Outcomes.COMPUTER_WINS;
+            game.incrementComputerWins()
+        }
+        else if (this.playerSelection == Selections.PAPER && this.computerSelection == Selections.ROCK) {
+            outcome = Outcomes.PLAYER_WINS
+            game.incrementPlayerWins()
+        }
+        else if (this.playerSelection == Selections.PAPER && this.computerSelection == Selections.SCISSORS){
+            outcome = Outcomes.COMPUTER_WINS
+            game.incrementComputerWins()
+        }   
+        else if (this.playerSelection == Selections.SCISSORS && this.computerSelection == Selections.PAPER){
+            outcome = Outcomes.PLAYER_WINS
+            game.incrementPlayerWins()
+        }
+        else if (this.playerSelection = Selections.SCISSORS && this.computerSelection == Selections.ROCK) {
+            outcome = Outcomes.COMPUTER_WINS
+            game.incrementComputerWins()
+        }
+        else if (this.playerSelection == this.computerSelection){
+            outcome = Outcomes.TIE
+            game.incrementCountOfTies()
+        }
+
+        return outcome
     }
 
 
@@ -48,4 +84,4 @@ class Round{
 
 
 //DO NOT TOUCH THIS LINE OF CODE//
-let unit_test1 = Round;
+let unit_test1 = Round; 
